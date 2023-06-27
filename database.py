e3fdf38d51f722982f79cddb3f4f558e855e90cf
SoobinJung <judyb0626@gmail.com>
2023-06-27 13:36:43 +0900

    refactor: Update environment config

--- database.py
+++ database.py
@@ -1,10 +1,11 @@
 import pymysql
-from config import DATABASE_HOST, DATABASE_USER, DATABASE_PASSWORD, DATABASE_NAME
+from config import DATABASE_HOST, DATABASE_USER, DATABASE_PASSWORD, DATABASE_NAME, DATABASE_PORT
+
 
 def connect_database():
     conn = pymysql.connect(
         host=DATABASE_HOST,
-        port=3306,
+        port=DATABASE_PORT,
         user=DATABASE_USER,
         password=DATABASE_PASSWORD,
         database=DATABASE_NAME
